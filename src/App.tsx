6ea3369706d7736d907c91ae7eee91e36701d6e2
Ugo Chiori <ugochiori@gmail.com>
2023-06-24 10:51:15 +0100

    All files

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useContext } from "react";
-import { Router, Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Home from "./landing/Home";
 import Signin from "./pages/Signin";
 import Signup from "./pages/Signup";
@@ -9,7 +9,6 @@ import { ErrorBoundary } from "react-error-boundary";
 import { AuthContext } from "./context/AuthContext";
 import HospitalDetails from "./hospital/HospitalDetails";
 import HealthTips from "./pages/HealthTips";
-// import PrivateRoute from "./utils/PrivateRoute";
 import { auth, signOut, signInWithGoogle } from "./firebase";
 import Navbar from "./navigation/Navbar";
 import Doctors from "./pages/Doctors";
@@ -39,7 +38,7 @@ function App() {
   };
 
   // Google signup
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+
   const signUp = () => {
     signUp();
   };
@@ -84,15 +83,17 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/signin" element={<Signin signIn={signIn} />} />
-          <Route path="/signup" element={<Signup />} />;
-          <Route
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/hospitals" element={<Hospitals handleDetails={handleDetails} />} />
+          {/* <Route
             path="/hospitals"
             element={
               <ProtectedRoute
                 element={<Hospitals handleDetails={handleDetails} />}
               />
             }
-          />
+          /> */}
+          
           <Route
             path="/hospitaldetails"
             element={
